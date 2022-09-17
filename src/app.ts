d9ea38da9e732c77e3c3286e66ff468a6872df1c
Eugene <eugene.popolamov@effective-soft.com>
2022-09-17 20:37:17 +0300

    feat: add auth routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import rootRoute from './routes/index';
 
 const app: Application = express();
 dotenv.config();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('combined'));
 app.use(cors());
+
+app.use('/', rootRoute);
 
 app.listen(process.env.PORT, () => {
   console.log('Connected to backend');
---
+++ src/controllers/auth.ts
@@ -0,0 +1,11 @@
+export const register = async () => {
+  return console.log('register');
+};
+
+export const login = async () => {
+  return console.log('login');
+};
+
+export const profile = async () => {
+  return console.log('profile');
+};
