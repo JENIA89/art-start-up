5a7ced3b4e05d6376dc0a3588b55054e5c1b7af7
Eugene <eugene.popolamov@effective-soft.com>
2022-09-17 14:51:19 +0300

    feat: add mogran

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app: Application = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(morgan('combined'));
 app.use(cors());
 
 app.listen(process.env.PORT, () => {
